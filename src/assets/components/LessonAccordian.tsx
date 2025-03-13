29f44b767c1f4d66eb3f420958e1c771880696cd
Bartavius <lojanarungsiri.j@northeastern.edu>
2025-03-13 16:00:35 -0400

    added component template for the lesson card in the dashboard and the lesson accordian in the lesson's homepage

---
+++ src/assets/components/LessonAccordian.tsx
@@ -0,0 +1,8 @@
+export default function LessonAccordian() {
+    return (
+        <div className="lesson-accordian-wrapper">
+            <div className="lesson-accordian-title">accordian title</div>
+            <div className="lesson-accordian-body">anything can be in the body fr</div>
+        </div>
+    )
+}
---
+++ src/assets/components/LessonCard.tsx
@@ -0,0 +1,14 @@
+export default function LessonCard() {
+    return (
+        <div className="lesson-card-wrapper">
+            <div className="lesson-icon">
+                icon
+            </div>
+            <div className="lesson-body">
+                <div className="lesson-title">lesson number: title</div>
+                <div className="lesson-description">shortened description</div>
+                <button className="btn-primary">primary button</button>
+            </div>
+        </div>
+    )
+}
